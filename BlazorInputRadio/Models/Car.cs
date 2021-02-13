e776dc1dac38c9c404d4c57522656d6e0e14b138
Rajeesh VP <rajeeshvp@gmail.com>
2021-02-13 17:41:20 +0530

    Fundamental solution

---
+++ BlazorInputRadio/Models/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorInputRadio.Models
+{
+    public class Car
+    {
+        [Required]
+        public Color Color { get; set; }// = Color.Black;
+        [Required]
+        public Model Model { get; set; } = Model.Model1;
+    }
+    public enum Color
+    {
+        Red, Black, White
+    }
+    public enum Model
+    {
+        Model1, Model2, Model3
+    }
+}
